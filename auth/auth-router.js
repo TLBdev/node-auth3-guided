1ba4d5c5fbe580f36a57cb6a619ff68fcacb71ae
tony <serratedhumor@yahoo.com>
2020-01-30 15:30:34 -0600

    mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken')
 const Users = require('../users/users-model.js');
+const { jwtSecret } = require('../configAuth/secrets')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,9 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+
+        const token = signToken(user)
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +37,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function signToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username
+  }
+
+
+  const option = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, jwtSecret, option)
+
+}
+
 
 module.exports = router;
